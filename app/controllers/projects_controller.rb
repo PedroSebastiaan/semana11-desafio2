d56dbf4da68563f9788f91eb1911e076299ab405
PedroSebastiaan <pizarropedrov@gmail.com>
2021-07-22 23:54:23 -0400

    prepare to heroku

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,17 @@
 class ProjectsController < ApplicationController
   def created
+    @project = Project.create(name: params[:name], description: params[:description], begin: params[:begin], end: params[:end], status: params[:status])
   end
 
   def new
+    @project = Project.new
   end
 
   def dashboard
+    if params[:status].present?
+      @projects = Project.where('status = ?', params[:status])
+    else
+    @projects = Project.all
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'projects/created'
+  post 'projects/created'
   get 'projects/new'
   get 'projects/dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
